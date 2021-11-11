af1a020bf5d0925f8cd8db0fd1a35bead23c994a
Fuchczyk <fuchczyk@debian-BULLSEYE-live-builder-AMD64>
2021-11-11 12:08:02 +0100

    Zmieniono obsługiwane rozszerzenia na .in oraz .out

--- README.md
+++ README.md
@@ -3,16 +3,18 @@
 ## Sposób uruchamiania
 
 1. Umieśić swój program w pliku project.c
-2. Dane wejściowe "in" umieścić w folderze '/tests/in'
-3. Dane wyjściowe "out" umieścić w folderze '/tests/out'
+2. Dane wejściowe "in" umieścić w folderze '/tests/in' (rozszerzenie .in)
+3. Dane wyjściowe "out" umieścić w folderze '/tests/out' (rozszerzenie .out)
 5. W folderze głównym uruchomić komendę 'bash testuj.sh'
 
 ## Uwagi do działania programu
 
 1. Pliki "in" i odpowiadające im "out" muszą mieć tę samą nazwę
-2. Zwrócić uwagę na komendy podane do kompilacji i weryfikacji dynamicznej pamięci ('gcc' oraz 'valgrind'), gdyż mogą być zmieniane w zależności od projektu
-3. Archiwalne testy do zadań będą przechowywane w folderze 'archive'
-4. Z powodu wczesnej wersji i braku doświadczenia w bashu, program może zawierać rażące błędy
+2. Pliku "in" muszą mieć rozszerzenie .in oraz pliki "out" muszą mieć nazwę .out
+3. Zwrócić uwagę na komendy podane do kompilacji i weryfikacji dynamicznej pamięci ('gcc' oraz 'valgrind'), gdyż mogą być zmieniane w zależności od projektu
+4. Archiwalne testy do zadań będą przechowywane w folderze 'archive'
+5. Z powodu wczesnej wersji i braku doświadczenia w bashu, program może zawierać rażące błędy
+6. Koniecznie należy używać do uruchamiania komendy 'bash' a nie komendy 'sh'.
 
 ## Kontrybucja
 
--- testuj.sh
+++ testuj.sh
@@ -2,6 +2,7 @@
 cd "$(dirname "$0")"
 echo ----KOMPILACJA----
 echo Kompiluję program projektu
+touch tmp
 gcc -std=c17 -pedantic -Wall -Wextra -Wformat-security -Wduplicated-cond -Wfloat-equal -Wshadow -Wconversion -Wjump-misses-init -Wlogical-not-parentheses -Wnull-dereference -Wvla -Werror -fstack-protector-strong -fsanitize=undefined -fno-sanitize-recover -g -fno-omit-frame-pointer -O1 project.c -o project
 
 if [ "$?" -ne "0" ]; then
@@ -14,16 +15,16 @@ echo ----TESTOWANIE----
 for file in tests/in/*
 do
     echo -"$(basename $file)"-
-    command=$(valgrind --leak-check=full -q --error-exitcode=1 ./project <$file | diff - tests/out/$(basename $file))
-    if [[ -z $command ]]; then
-        printf "Pomyślnie ukończono próbę "$(basename $file)"."
+    command=$(valgrind --leak-check=full -q --error-exitcode=1 ./project <$file | diff - tests/out/$(basename -s .in $file).out)
+    if [ -z $command ]; then
+        printf "Pomyślnie ukończono próbę "$(basename -s .in $file)"."
         time(./project <$file >tmp)
         echo
     else
         echo !-!-! Błąd !-!-!
         echo Błędny wynik podczas wykonywania próby "$(basename $file)"
         echo Spodziewany wynik: 
-        cat tests/out/$(basename $file)
+        cat tests/out/$(basename -s .in $file).out
         echo Otrzymany wynik:
         valgrind --leak-check=full -q --error-exitcode=1 ./project <$file
         break
